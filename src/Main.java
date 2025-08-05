2ae78c71e3779bccc845801b1a5ef669faf4c19c
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-05 16:52:48 -0300

    exemplos aula

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 24;
+        float height = 1.80f;
+        char gender = 'M';
+        boolean special = false;
+        String name = "John Doe";
+    }
+}
