9d5d915b8648b53fb737addaf7ca583ae99365d8
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-06 14:15:16 -0300

    Exemplos aula

---
+++ src/VariaveisExemplo.java
@@ -0,0 +1,15 @@
+public class VariaveisExemplo {
+
+    public static void main(String[] args) {
+        int idade = 30;
+        double altura = 1.75;
+        char genero = 'M';
+        boolean ehEstudante = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Genero: " + genero);
+        System.out.println("É estudante? " + ehEstudante);
+    }
+
+}
